b8a96bdaaffe4e3277ede4bb46a762f0409f54a6
Sidiq Indra Gunawan <sidiqindra@student.telkomuniversity.ac.id>
2021-09-27 00:06:10 +0700

    HelloWorld.java

---
+++ Praktikum2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+ public static void main(String[] args) {
+  System.out.println("Hello Sidiq");
+  System.out.println("6706210108");
+  System.out.println("D3RPLA-45-01");
+ }
+}
